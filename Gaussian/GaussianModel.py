668e674bc861b8de33333e7d0a83920c170bf310
tatuya <nakao.tatuya.2000@gmail.com>
2023-11-30 20:16:08 +0900

    modified all code

--- Gaussian/GaussianModel.py
+++ Gaussian/GaussianModel.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report
+import matplotlib.pylab as plt
 
 datas = np.loadtxt("iris.csv")
 #print(datas)
@@ -9,13 +10,86 @@ X = datas[:,0:4]
 #print(X)
 Y = datas[:,-1].astype("int32")
 #print(Y)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, shuffle=True, random_state=3)
-
 G = GaussianNB(priors=None, var_smoothing=1e-09)
+cross_num = 20
+random_seed = np.arange(cross_num)
 
-G.fit(X_train, Y_train)
-G.get_params(deep=True)
+class ResultData:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.f1 = list()
+        self.precision = list()
+        self.recall = list()
+    
+    def add_data(self, f1, pre, rec):
+        self.f1.append(f1)
+        self.precision.append(pre)
+        self.recall.append(rec)
 
-Y_pred_GNB = G.predict(X_test)
-#print(Y_pred_GNB)
-print(classification_report(Y_test, Y_pred_GNB))
+    def __str__(self) -> str:
+        f1_m = np.mean(self.f1)
+        f1_v = np.var(self.f1)
+        f1_str = "f1 mean: " + str(f1_m) + ", f1 var: " + str(f1_v)
+        pr_m = np.mean(self.precision)
+        pr_v = np.var(self.precision)
+        pr_str = "precision mean: " + str(pr_m) + ", precision var: " + str(pr_v)
+        re_m = np.mean(self.recall)
+        re_v = np.var(self.recall)
+        re_str = "recall mean: " + str(re_m) + ", recall var: " + str(re_v)
+        return self.name + "\n\t" + f1_str + "\n\t" + pr_str + "\n\t" + re_str
+
+setosa = ResultData("setosa")
+versicolor = ResultData("versicolor")
+virginica = ResultData("virginica")
+
+for s in random_seed:
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True, random_state=s)
+    G.fit(X_train, Y_train)
+    Y_pred_GNB = G.predict(X_test)
+    res = classification_report(Y_test, Y_pred_GNB, output_dict=True)
+    setosa.add_data(res['0']["f1-score"], res['0']["precision"], res['0']["recall"])
+    versicolor.add_data(res['1']["f1-score"], res['1']["precision"], res['1']["recall"])
+    virginica.add_data(res['2']["f1-score"], res['2']["precision"], res['2']["recall"])
+
+print(setosa)
+print(versicolor)
+print(virginica)
+
+def Normal(x, theta, var):
+    return (1/np.sqrt(2*np.pi*var))*np.exp(-(x-theta)**2 / (2*var**2))
+
+print("theta:")
+print(G.theta_)
+print("var:")
+print(G.var_)
+print()
+theta = G.theta_
+var = G.var_
+
+X = []
+Y = []
+for c_t, c_v in zip(theta, var):
+    add_x = []
+    add_y = []
+    for p_t, p_v in zip(c_t, c_v):
+        add_x.append(np.linspace(p_t-1, p_t+1, 200))
+        add_y.append([Normal(x, p_t, p_v) for x in add_x[-1]])
+    X.append(add_x)
+    Y.append(add_y)
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+class_num = 3
+param_num = 4
+for p in range(param_num):
+    for c in range(class_num):
+        ax[p//2,p%2].plot(X[c][p], Y[c][p])
+        if p==0:
+            ax[p//2,p%2].set_title("sepal_Length")
+        if p==1:
+            ax[p//2,p%2].set_title("sepal_Width")
+        if p==2:
+            ax[p//2,p%2].set_title("Petal_Length")
+        if p==3:
+            ax[p//2,p%2].set_title("Petal_Width")
+
+plt.show()
--- Multinomial/MultinomialModel.py
+++ Multinomial/MultinomialModel.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+import pprint
+import matplotlib.pylab as plt
 
 datas = np.loadtxt("iris.csv")
 #print(datas)
@@ -9,13 +11,98 @@ X = datas[:,0:4]
 #print(X)
 Y = datas[:,-1].astype("int32")
 #print(Y)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, shuffle=True, random_state=3)
-
 M = MultinomialNB(alpha=1.0, fit_prior=True, class_prior=None)
+cross_num = 20
+random_seed = np.arange(cross_num)
 
-M.fit(X_train, Y_train)
-M.get_params(deep=True)
+class ResultData:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.f1 = list()
+        self.precision = list()
+        self.recall = list()
+    
+    def add_data(self, f1, pre, rec):
+        self.f1.append(f1)
+        self.precision.append(pre)
+        self.recall.append(rec)
 
-Y_pred_GNB = M.predict(X_test)
-#print(Y_pred_GNB)
-print(classification_report(Y_test, Y_pred_GNB))
+    def __str__(self) -> str:
+        f1_m = np.mean(self.f1)
+        f1_v = np.var(self.f1)
+        f1_str = "f1 mean: " + str(f1_m) + ", f1 var: " + str(f1_v)
+        pr_m = np.mean(self.precision)
+        pr_v = np.var(self.precision)
+        pr_str = "precision mean: " + str(pr_m) + ", precision var: " + str(pr_v)
+        re_m = np.mean(self.recall)
+        re_v = np.var(self.recall)
+        re_str = "recall mean: " + str(re_m) + ", recall var: " + str(re_v)
+        return self.name + "\n\t" + f1_str + "\n\t" + pr_str + "\n\t" + re_str
+
+setosa = ResultData("setosa")
+versicolor = ResultData("versicolor")
+virginica = ResultData("virginica")
+
+accuracy = 0.0
+most_acc_result = None
+for s in random_seed:
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True, random_state=s)
+    M.fit(X_train, Y_train)
+    Y_pred_GNB = M.predict(X_test)
+    res = classification_report(Y_test, Y_pred_GNB, output_dict=True)
+    #pprint.pprint(res)
+    #if accuracy < res["accuracy"]:
+    #    most_acc_result = res
+    setosa.add_data(res['0']["f1-score"], res['0']["precision"], res['0']["recall"])
+    versicolor.add_data(res['1']["f1-score"], res['1']["precision"], res['1']["recall"])
+    virginica.add_data(res['2']["f1-score"], res['2']["precision"], res['2']["recall"])
+
+seed = 0
+while(True):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True, random_state=seed)
+    M.fit(X_train, Y_train)
+    Y_pred_GNB = M.predict(X_test)
+    res = classification_report(Y_test, Y_pred_GNB, output_dict=True)
+    if 0.9 < res["accuracy"]:
+        break
+    seed += 1
+
+print(res["accuracy"])
+print(setosa)
+print(versicolor)
+print(virginica)
+
+test_X0 = np.array([ np.array([i, 0.0, 0.0, 0.0]) for i in np.linspace(4, 8, 20) ])
+test_X1 = np.array([ np.array([i, 0.0, 0.0, 0.0]) for i in np.linspace(2, 5, 20) ])
+test_X2 = np.array([ np.array([i, 0.0, 0.0, 0.0]) for i in np.linspace(1, 7, 20) ])
+test_X3 = np.array([ np.array([i, 0.0, 0.0, 0.0]) for i in np.linspace(0.1, 2.5, 20) ])
+
+pred_Y0 = M.predict_proba(test_X0).T
+pred_Y1 = M.predict_proba(test_X1).T
+pred_Y2 = M.predict_proba(test_X2).T
+pred_Y3 = M.predict_proba(test_X3).T
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+
+ax[0, 0].set_title("sepal_Length")
+ax[0, 1].set_title("sepal_Width")
+ax[1, 0].set_title("Petal_Length")
+ax[1, 1].set_title("Petal_Width")
+
+ax[0, 0].plot(np.linspace(4, 8, 20), pred_Y0[0])
+ax[0, 0].plot(np.linspace(4, 8, 20), pred_Y0[1])
+ax[0, 0].plot(np.linspace(4, 8, 20), pred_Y0[2])
+
+ax[0, 1].plot(np.linspace(2, 5, 20), pred_Y1[0])
+ax[0, 1].plot(np.linspace(2, 5, 20), pred_Y1[1])
+ax[0, 1].plot(np.linspace(2, 5, 20), pred_Y1[2])
+
+ax[1, 0].plot(np.linspace(1, 7, 20), pred_Y2[0])
+ax[1, 0].plot(np.linspace(1, 7, 20), pred_Y2[1])
+ax[1, 0].plot(np.linspace(1, 7, 20), pred_Y2[2])
+
+ax[1, 1].plot(np.linspace(0.1, 2.5, 20), pred_Y3[0])
+ax[1, 1].plot(np.linspace(0.1, 2.5, 20), pred_Y3[1])
+ax[1, 1].plot(np.linspace(0.1, 2.5, 20), pred_Y3[2])
+
+plt.show()
